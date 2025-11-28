11ec4c5182c122af0bac0b98045e1ca4b1f45c3d
unknown <harshgoel2004.work@gmail.com>
2025-11-29 00:45:57 +0530

    adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -132,11 +132,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5202236007367090"
-      crossOrigin="anonymous"
-    />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5202236007367090"
+     crossOrigin="anonymous"></script>
       </head>
       <body
         className={`${geistSans.variable} antialiased`}
